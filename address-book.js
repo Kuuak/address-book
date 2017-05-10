b6ed486ea57bf4ec3acdbf7e7b255479045b49d6
Felipe <felipe.paulmartins@hotmail.com>
2017-05-11 00:39:06 +0200

    Add Express config, define API routes & basic html

---
+++ address-book.js
@@ -0,0 +1,34 @@
+// Server, Express & Socket.io
+const express	= require( 'express' );
+const app			= express();
+const server	= require( 'http' ).Server( app );
+
+// Express Middlewares
+const bodyParser = require('body-parser');
+
+/**
+ * Express config & routes handling
+ */
+app
+	.use( express.static( __dirname +'/public' ) )
+	.use( bodyParser.urlencoded({ extended: true }) )
+
+	// ROUTES
+	.get( '/', (req, res) => {
+		res.sendFile( __dirname +'/public/index.html' );
+	} )
+	.get( '/search/:number', (req, res) => {} )
+	.post( '/costumer/add/', (req, res) => {} )
+	.post( '/costumer/edit/', (req, res) => {} )
+	.post( '/costumer/add/address/', (req, res) => {} )
+	.post( '/costumer/edit/address/', (req, res) => {} )
+	.post( '/costumer/delete/', (req, res) => {} )
+	.post( '/costumer/delete/address/', (req, res) => {} )
+
+	// Redirect to root if request does not match any route
+	.use( '/', (req, res) => res.redirect( '/' ) );
+
+/**
+ * Start server
+ */
+server.listen( 8080 );
