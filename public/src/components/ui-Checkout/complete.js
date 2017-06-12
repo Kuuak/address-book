8e56d44632d556555ab62e64486f9a0b72177328
Felipe <felipe.paulmartins@hotmail.com>
2017-06-12 20:24:45 +0200

    Add printing 2 copies on checkout complete page Add --kiosk-printing option in Chrome to print "almost" silently

--- public/src/components/ui-Checkout/complete.js
+++ public/src/components/ui-Checkout/complete.js
@@ -1,6 +1,9 @@
 // Assets depedencies (Style & images)
 import './complete.css';
 
+// APP settings
+import config from '../../../../config';
+
 // React
 import React from 'react';
 import { Link } from 'react-router-dom';
@@ -15,10 +18,46 @@ export default class CheckoutComplete extends React.Component {
 		super( props );
 
 		this.handlePrint = this.handlePrint.bind( this );
+		this.onAfterPrint = this.onAfterPrint.bind( this );
+		this.mediaQueryListener = this.mediaQueryListener.bind( this );
+	}
+
+	componentDidMount() {
+
+		if (window.matchMedia) {
+			this.mediaQueryList = window.matchMedia('print');
+			this.mediaQueryList.addListener( this.mediaQueryListener );
+		}
+
+		window.onafterprint = this.onAfterPrint;
+	}
+
+	componentWillUnmount() {
+		if (window.matchMedia) {
+			this.mediaQueryList.removeListener( this.mediaQueryListener );
+		}
+		window.onafterprint = null;
+	}
+
+	mediaQueryListener( mql ) {
+		if (!mql.matches) {
+			this.onAfterPrint();
+		}
+	}
+
+	onAfterPrint( a, b ) {
+		this.numberPrinted++;
+		if ( !isNaN(this.numberPrinted) && this.numberPrinted < config.print.number ) {
+			window.print();
+		}
+		else {
+			this.numberPrinted = undefined;
+		}
 	}
 
 	handlePrint( event ) {
 		event.preventDefault();
+		this.numberPrinted = 0;
 		window.print();
 	}
 
@@ -29,12 +68,12 @@ export default class CheckoutComplete extends React.Component {
 				<div className="content">
 					<h1>Confirmation</h1>
 					<div className="checkout-process-action top">
-						<Link to="/print/" onClick={ this.handlePrint } className="btn red checkout-print">Imprimer</Link>
+						<Link to="/print/" onClick={ this.handlePrint } className="btn red checkout-print">Imprimer (2 copies)</Link>
 					</div>
 					{ this.props.active && <Order id={ this.props.id } addAlerts={this.props.addAlerts } /> }
 					<div className="checkout-process-action">
 						<Link to="/" className="btn red lighten-2 checkout-return">Fermer</Link>
-						<Link to="/print/" onClick={ this.handlePrint } className="btn red checkout-print">Imprimer</Link>
+						<Link to="/print/" onClick={ this.handlePrint } className="btn red checkout-print">Imprimer (2 copies)</Link>
 					</div>
 				</div>
 			</section>
